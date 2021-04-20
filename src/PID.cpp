a59ff6756ec56439d41dedb3f0004c5e18f66ccf
Albert Qianyi Fu <qifu@umich.edu>
2021-04-19 20:48:31 -0400

    Initial commit

---
+++ src/PID.cpp
@@ -0,0 +1,38 @@
+#include "PID.h"
+
+/**
+ * TODO: Complete the PID class. You may add any additional desired functions.
+ */
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp_, double Ki_, double Kd_) {
+  /**
+   * TODO: Initialize PID coefficients (and errors, if needed)
+   */
+  Kp = Kp_;
+  Ki = Ki_;
+  Kd = Kd_;
+
+  p_error = 0.0;
+  i_error = 0.0;
+  d_error = 0.0;
+
+  prev_cte = 0.0;
+}
+
+void PID::UpdateError(double cte) {
+  p_error = cte;
+  i_error += cte;
+  d_error = cte - prev_cte;
+  prev_cte = p_error;
+}
+
+double PID::TotalError() {
+  /**
+   * TODO: Calculate and return the total error
+   */
+  return -((Kp * p_error) + (Ki * i_error) + (Kd * d_error));
+}
